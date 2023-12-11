263cbd4edd2366adba41710d745b377a6ce61808
Ghanshyam Verma <ghanshyam9161@gmail.com>
2023-12-11 16:28:10 +0530

    first application

---
+++ Legalcore/src/main/java/com/ghanshyam/controller/GetPhoto.java
@@ -0,0 +1,32 @@
+package com.incapp.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.incapp.model.DAO;
+
+/**
+ * Servlet implementation class AdminLogin
+ */
+@WebServlet("/GetPhoto")
+public class GetPhoto extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			String email=request.getParameter("email");
+			String type=request.getParameter("type");
+			DAO d=new DAO();
+			byte b[]=d.getPhoto(email, type);
+			response.getOutputStream().write(b);
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
